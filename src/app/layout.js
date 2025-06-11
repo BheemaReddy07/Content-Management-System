c5b21cc75a9752bf047e27cfb316e7f530ee1a36
BheemaReddy07 <bheemareddy2910@gmail.com>
2025-06-11 18:59:08 +0530

    Landing page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,16 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sideba";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+ import {Poppins} from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  
+  weight: [ "400","500","600"],
+})
+
 
 export const metadata = {
   title: "Create Next App",
@@ -19,11 +19,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="w-full">
+            <SidebarTrigger />
+                {children}
+          </main>
+        </SidebarProvider>
+        
       </body>
     </html>
   );
